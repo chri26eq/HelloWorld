c135c429f9cae2d4db0026e7ec856764a4bb14c9
chri26eq <chri26eq@stud.kea.dk>
2023-09-08 10:50:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+        double test1 = 10.2;
+        double test2 = 10.2;
+        double test3 = 10.2;
+        double test4 = 10.2;
+        double test5 = 10.2;
+        double test6 = 10.2;
+        double test7 = 10.2;
+        double test8 = 10.2;
+        double test9 = 10.2;
+        double test10 = 10.2;
+    }
+}
